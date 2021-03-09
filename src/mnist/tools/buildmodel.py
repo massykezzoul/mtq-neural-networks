fe393eed092f682378f8ff4b6b44635e42f12088
chakib <celhouiti@gmail.com>
2021-03-09 02:03:48 +0100

    merge

--- src/mnist/tools/buildmodel.py
+++ src/mnist/tools/buildmodel.py
@@ -8,8 +8,8 @@ from tensorflow.keras.optimizers import Adam
 
 class MTQModel():
     model_name = None
-    model = None
-
+    model = None 
+    
     def __init__(self, model_name=None):
         self.model_name = model_name
 
